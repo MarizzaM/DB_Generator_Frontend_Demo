f08803ab97fbfd988def4b63c4e09f510c19536f
Pavlova Marina <marizza.mil89@gmail.com>
2021-12-21 14:35:42 +0200

    Labels_Input_and_GUI_Layouts

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2
+
+        self.add_widget(Label(text="Airline Companies: "))
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+
+        self.add_widget(Label(text="Customers: "))
+        self.lastName = TextInput(multiline=False)
+        self.add_widget(self.lastName)
+
+        self.add_widget(Label(text="Administrators: "))
+        self.email = TextInput(multiline=False)
+        self.add_widget(self.email)
+
+        self.add_widget(Label(text="Flights Per Company: "))
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+
+        self.add_widget(Label(text="Tickets Per Customer: "))
+        self.lastName = TextInput(multiline=False)
+        self.add_widget(self.lastName)
+
+        self.add_widget(Label(text="Countries: "))
+        self.email = TextInput(multiline=False)
+        self.add_widget(self.email)
 
 
 class MyApp(App):
     def build(self):
-        return Label(text="Hello world")
+       return MyGrid()
 
 
 if __name__ == "__main__":
